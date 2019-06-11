cce79dd3dcd71e163ff8dee66356cdcd3bdaf418
MaksimTitenko <maksim_9222@mail.ru>
2019-06-11 17:49:48 +0300

    Zen python added

---
+++ task.py
@@ -0,0 +1,10 @@
+print('''
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+''')
